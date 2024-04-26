09d4ba2bbf68a89a3b59a39e97383c6d2e33d12e
NeugeMa <mariana.n.dourado@gmail.com>
2024-04-25 23:05:45 -0300

    Produção do formulário de contato (css + index + js) | commit mari

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 
 // Verificação de Idade
 function confirmAge() {
-   var isOver18 = confirm("Você tem mais de 18 anos?");
-   if (isOver18) {
-       window.location.href = "home.html"; // Página inicial
-   } else {
-       window.location.href = "https://github.com/Jython-FIAP/CP2-WebDev"; // Site para menores de 18 anos
-   }
+    var isOver18 = confirm("Você tem mais de 18 anos?");
+    if (isOver18) {
+        window.location.href = "home.html"; // Página inicial
+    } else {
+        window.location.href = "https://github.com/Jython-FIAP/CP2-WebDev"; // Site para menores de 18 anos
+    }
+}
+
+// -------------------------------- // 
+
+// Formulário de Contato 
+document.getElementById('formulario').onsubmit = function(e) {
+    e.preventDefault(); 
+    alert("Sua mensagem foi enviada! Ficamos feliz com seu feedback :)");
 }
 
 // -------------------------------- // 
